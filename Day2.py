29ae9f05a4a57730d60356f671ee50832f4a5fdc
LightxAman <amanxlight@gmail.com>
2024-02-10 21:35:38 +0530

    attempt 1

---
+++ Day2.py
@@ -0,0 +1,11 @@
+# Title : Modules and usage of PIP
+
+# Types of Modules : Builtin Module and External Module
+
+# Builtin Modules
+
+import hashlib
+
+# External Modules
+
+import pandas
---
+++ Day3.py
@@ -0,0 +1,15 @@
+# Title : Print statement
+
+# Printing String
+print("Hello world")
+
+# Printing Integer value
+print(5)
+
+# Printing a combinations of string and int
+print("Yo", 4)
+
+# Printing simple calculation in print statement
+print(17 * 13)
+
+print("bye")
