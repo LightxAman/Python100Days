8a8e424b66294cd06e59e841df03e10a588030f5
Aman Sharma <118755943+LightxAman@users.noreply.github.com>
2024-02-10 21:31:33 +0530

    Add files via upload

---
+++ Python100Days/Day1.py
@@ -0,0 +1,4 @@
+# Title : Going over python basics
+
+print("Hello World")
+print(5)
---
+++ Python100Days/Day4.py
@@ -0,0 +1,24 @@
+# Title : Comments, Escape character sequences and print statement
+
+# This print statement will throw an error because we entered a new line
+
+# print("Hey , I am a good boy,
+#       and you are a good boy too")
+
+#  to resolve this use "escape sequence characters eg \n"
+
+print("Hey , I am a good boy\nand you are a good boy too")
+
+# This introduces a new line
+
+# This is a single line comment
+print("Hello World")  # A comment can be added after the statements as well
+
+# A multiline comment can also be done by using """ """ or ''' '''
+'''
+Yo this is all a multiline comment example
+never mind the text written 
+a lot of my mind is 
+chipi chipi chapa chapa 
+dubi dubi daba daba
+'''
