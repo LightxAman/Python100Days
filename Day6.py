126a4603f903d10c5a1c1a64261584386716201d
LightxAman <amanxlight@gmail.com>
2024-02-12 17:08:53 +0530

    Day6 commit
    Signed-off-by: LightxAman <amanxlight@gmail.com>

--- Day6.py
+++ Day6.py
@@ -1,2 +1,22 @@
-# yo
-# sdasd
+# Operators
+
+# ADD operator
+print(15 + 6)
+
+# SUBTRACT operator
+print(15 - 6)
+
+# MULTIPLY operator
+print(15 * 6)
+
+# DIVISION operator
+print(15 / 6)
+
+# FLOOR DIVISION operator - It disregards the floating point value and takes the int.
+print(15 // 6)
+
+# MODULUS operator
+print(15 % 6)
+
+# EXPONENTIAL operator - Basically is power of.
+print(15 ** 6)
---
+++ Day7.py
@@ -0,0 +1,15 @@
+# A simple calculator
+# This simple program assumes that the first no is always greater for smoother flow.
+
+a = 10
+b = 2
+
+print("The numbers are : ", a, ", ", b)
+print("\nADDITION of A + B :", a + b)
+print("\nSUBTRACTION of A - B :", a - b)
+print("\nMULTIPLICATION of A * B :", a * b)
+print("\nDIVISION of A / B :", a / b)
+print("\nMODULUS of A % B :", a % b)
+print("\nEXPONENTIAL of A raised to power B :", a ** b)
+
+print("\nThank You!")
