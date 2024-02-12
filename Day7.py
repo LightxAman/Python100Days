d7bc7a83d114f7e6929851dfe8d1dfd97822f6e2
LightxAman <amanxlight@gmail.com>
2024-02-12 17:09:55 +0530

    Day6 commit
    Signed-off-by: LightxAman <amanxlight@gmail.com>

--- Day7.py
+++ Day7.py
@@ -5,11 +5,17 @@ a = 10
 b = 2
 
 print("The numbers are : ", a, ", ", b)
+
 print("\nADDITION of A + B :", a + b)
+
 print("\nSUBTRACTION of A - B :", a - b)
+
 print("\nMULTIPLICATION of A * B :", a * b)
+
 print("\nDIVISION of A / B :", a / b)
+
 print("\nMODULUS of A % B :", a % b)
+
 print("\nEXPONENTIAL of A raised to power B :", a ** b)
 
 print("\nThank You!")
