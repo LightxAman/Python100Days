ec8c77ae61e9484051b470486c5e62d6a9e90715
LightxAman <amanxlight@gmail.com>
2024-02-12 17:29:30 +0530

    Day6 commit
    Signed-off-by: LightxAman <amanxlight@gmail.com>

---
+++ Day8.py
@@ -0,0 +1,9 @@
+# Type Casting
+
+# Problem
+a = "1"
+b = "2"
+print(
+    a + b)  # this will show output as 12 because python is treating 1 and 2 as strings and implicitly converting them.
+
+# TypeCasting : The coversion of one datatype into another type is known as type casting
