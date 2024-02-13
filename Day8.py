3c884c954f7b8b6d5440baacb1dcc4506541336a
LightxAman <amanxlight@gmail.com>
2024-02-13 19:12:15 +0530

    Day7 and 8 commit
    Signed-off-by: LightxAman <amanxlight@gmail.com>

--- Day8.py
+++ Day8.py
@@ -6,4 +6,17 @@ b = "2"
 print(
     a + b)  # this will show output as 12 because python is treating 1 and 2 as strings and implicitly converting them.
 
-# TypeCasting : The coversion of one datatype into another type is known as type casting
+# TypeCasting : The conversion of one datatype into another type is known as type casting
+
+print(int(a) + int(b))  # typecasting into INT
+
+# Types : Explicit Conversion and Implicit Conversion
+
+print(int(a) + int(b))  # Explicit typecasting into INT
+
+# Implicit Typecasting
+
+c = 1.9
+d = 5
+
+print(c + d)  # Python interpreter converts lower datatype into higher datatype automatically
