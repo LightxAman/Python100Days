689deaec473f426d1f910f71b2a708df77ab0bbf
LightxAman <amanxlight@gmail.com>
2024-02-15 11:58:59 +0530

    5th day
    Signed-off-by: LightxAman <amanxlight@gmail.com>

---
+++ Day11.py
@@ -0,0 +1,20 @@
+# String Slicing
+
+name = "Light,Dark"
+print(name[0:5])  # including 0 but not 5
+
+# Length of a string
+
+print(len(name))
+
+# String as an array
+
+print(name[:5])
+
+print(name[:])  # by default, it takes string length
+
+print(name[4])
+
+print(name[0:-3])  # i.e, print(name[0: len(name)-3])
+
+print(name[-3:-1])
