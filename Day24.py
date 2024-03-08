9680552617a62319d082f70367a4ed5676cc4822
LightxAman <amanxlight@gmail.com>
2024-03-08 20:36:23 +0530

    22th day
    Signed-off-by: LightxAman <amanxlight@gmail.com>

--- Day24.py
+++ Day24.py
@@ -1,9 +1,16 @@
 # Python Practice examples on Loops
 
-print("\nExample 1")
+print("\nExample 1 : access elements of the list one by one")
 
 languages = ['Swift', 'Python', 'Go']
 
 # access elements of the list one by one
 for i in languages:
     print(i)
+
+print("\nExample 1 : iterate over each character in language")
+language = 'Python'
+
+# iterate over each character in language
+for x in language:
+    print(x)
