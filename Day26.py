2ca3ac6e4ff3337e967ca8cfc905bedd0400cacf
LightxAman <amanxlight@gmail.com>
2024-03-11 18:05:19 +0530

    24th day
    Signed-off-by: LightxAman <amanxlight@gmail.com>

---
+++ Day26.py
@@ -0,0 +1,17 @@
+# Tuples
+# Tuples are ordered collections of data item.
+# They store multiple items in a single variable.
+# Tuple items are separated by commas and enclosed within round brackets().
+# Tuples are unchangeable meaning we can not alter them after creation.
+
+tup = (1, 5, 6)
+print(type(tup), tup)
+
+# Here python confuses and takes this as an int.
+tup1 = (1)
+print(type(tup1), tup1)
+
+# So to solve this issue we add comma
+
+tup2 = (1,)
+print(type(tup2), tup2)
