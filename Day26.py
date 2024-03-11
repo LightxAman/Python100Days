5d940a9e0b61d62f29a2dd9c3d406f6841c688fe
LightxAman <amanxlight@gmail.com>
2024-03-11 18:08:32 +0530

    24th day
    Signed-off-by: LightxAman <amanxlight@gmail.com>

--- Day26.py
+++ Day26.py
@@ -4,7 +4,7 @@
 # Tuple items are separated by commas and enclosed within round brackets().
 # Tuples are unchangeable meaning we can not alter them after creation.
 
-tup = (1, 5, 6)
+tup = (1, 5, 6, 6, 7, 8, 9, True)
 print(type(tup), tup)
 
 # Here python confuses and takes this as an int.
@@ -15,3 +15,10 @@ print(type(tup1), tup1)
 
 tup2 = (1,)
 print(type(tup2), tup2)
+
+# Slicing
+tup3 = tup[1:4]
+print(tup3)
+
+if 8 in tup:
+    print("Yes, It exists!")
