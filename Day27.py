fe16fe870470253a0b7798099a21bd007bfd191e
LightxAman <amanxlight@gmail.com>
2024-03-12 21:08:04 +0530

    24th day
    Signed-off-by: LightxAman <amanxlight@gmail.com>

--- Day27.py
+++ Day27.py
@@ -10,3 +10,16 @@ temp.pop(3)
 temp[2] = "Finland"
 countries = tuple(temp)
 print(countries)
+
+# Examples of tuples
+languages = ('Python', 'Swift', 'C++')
+
+# access the first item
+print(languages[0])  # Python
+
+# access the third item
+print(languages[2])  # C++
+
+# Python Tuple Length
+cars = ('BMW', 'Tesla', 'Ford', 'Toyota')
+print('Total Items:', len(cars))
