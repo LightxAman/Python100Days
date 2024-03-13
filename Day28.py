e9a9000f7e5c5da6caea4b25805b8ad7cfe9c09b
LightxAman <amanxlight@gmail.com>
2024-03-13 18:03:58 +0530

    26th day
    Signed-off-by: LightxAman <amanxlight@gmail.com>

--- Day28.py
+++ Day28.py
@@ -2,3 +2,9 @@
 # Create a program capable of displaying questions to the users like KBC.
 # Use LIST data type to store the questions and their answers.
 # Display  the final amount the person is taking home after playing the game.
+
+
+def display_question(question, options):
+    print(question)
+    for i, option in enumerate(options, start=1):
+        print(f"{i}. {option}")
