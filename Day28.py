9c82de8bb227ac0023e8d74a7f7e33519dbcd320
LightxAman <amanxlight@gmail.com>
2024-03-13 18:00:35 +0530

    26th day
    Signed-off-by: LightxAman <amanxlight@gmail.com>

--- Day28.py
+++ Day28.py
@@ -1,3 +1,4 @@
 # Exercise
 # Create a program capable of displaying questions to the users like KBC.
 # Use LIST data type to store the questions and their answers.
+# Display  the final amount the person is taking home after playing the game.
