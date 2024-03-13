c9120b435f627819a67e8e5d0c8930c6e9ca7b58
LightxAman <amanxlight@gmail.com>
2024-03-13 18:04:20 +0530

    26th day
    Signed-off-by: LightxAman <amanxlight@gmail.com>

--- Day28.py
+++ Day28.py
@@ -31,3 +31,19 @@ def kbc_game(questions):
         current_question += 1
 
     print(f"Congratulations! You've completed the game. Total amount won: {amount}")
+
+
+# Define the questions and answers in a list
+questions = [
+    ("What is the capital of France?", ["1. Paris", "2. London", "3. Berlin", "4. Rome"], 1, 1000),
+    ("Who wrote 'Romeo and Juliet'?",
+     ["1. William Shakespeare", "2. Jane Austen", "3. Charles Dickens", "4. Mark Twain"], 1, 2000),
+    ("What is the chemical symbol for water?", ["1. Wo", "2. Wa", "3. H2O", "4. We"], 3, 5000),
+    ("Who painted the Mona Lisa?",
+     ["1. Pablo Picasso", "2. Leonardo da Vinci", "3. Vincent van Gogh", "4. Michelangelo"], 2, 10000),
+    ("What is the largest mammal in the world?", ["1. Elephant", "2. Blue Whale", "3. Giraffe", "4. Hippopotamus"], 2,
+     20000)
+]
+
+# Start the game
+kbc_game(questions)
