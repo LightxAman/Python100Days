4499c8b252746a14484c8910e5fe3a388fafd490
LightxAman <amanxlight@gmail.com>
2024-03-16 21:18:53 +0530

    28th day
    Signed-off-by: LightxAman <amanxlight@gmail.com>

--- Day29.py
+++ Day29.py
@@ -5,3 +5,8 @@
 
 txt = "For only {price:.2f} dollars!"
 print(txt.format(price=49))
+
+# f-strings in python
+# It is a new string formatting mechanism introduced by the PEP 498.
+# It is aka Literal string Interpolation or more commonly as f-string.
+# The primary focus of this mechanism is to make the interpolation easier.
