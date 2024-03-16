1c025b9dc648eeff77bf1a152361051ad99fa7d5
LightxAman <amanxlight@gmail.com>
2024-03-16 21:20:17 +0530

    28th day
    Signed-off-by: LightxAman <amanxlight@gmail.com>

--- Day29.py
+++ Day29.py
@@ -10,3 +10,5 @@ print(txt.format(price=49))
 # It is a new string formatting mechanism introduced by the PEP 498.
 # It is aka Literal string Interpolation or more commonly as f-string.
 # The primary focus of this mechanism is to make the interpolation easier.
+
+# When we prefix the string with the letter 'f', the string becomes the string itself.
