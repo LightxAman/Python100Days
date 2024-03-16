2a450b8767b4623ca11fb2e62fb50505556befa3
LightxAman <amanxlight@gmail.com>
2024-03-16 21:35:30 +0530

    28th day
    Signed-off-by: LightxAman <amanxlight@gmail.com>

--- Day30.py
+++ Day30.py
@@ -1,3 +1,13 @@
 # Docstrings in Python
 # docstrings are the string literals that appear right agter the definition of a fuction,
 # method, class, or module
+
+def square(n):
+    """Takes in a number n, returns the
+    square of n"""
+    print(n ** 2)
+
+
+square(5)
+# To see the docstring.
+print(square.__doc__)
