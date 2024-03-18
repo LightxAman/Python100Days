28775e41b3bec98d4e8f91775eb5266d6177e471
LightxAman <amanxlight@gmail.com>
2024-03-18 20:17:00 +0530

    30th day
    Signed-off-by: LightxAman <amanxlight@gmail.com>

--- Day30.py
+++ Day30.py
@@ -34,3 +34,20 @@ print(my_function.__doc__)
 
 print("Using help:")
 help(my_function)
+
+
+def multiply_numbers(a, b):
+    """
+	Multiplies two numbers and returns the result.
+
+	Args:
+		a (int): The first number.
+		b (int): The second number.
+
+	Returns:
+		int: The product of a and b.
+	"""
+    return a * b
+
+
+print(multiply_numbers(3, 5))
