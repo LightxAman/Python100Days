f48d776647ebc6861590c3fa81c934d4b5383f59
LightxAman <amanxlight@gmail.com>
2024-03-19 20:03:03 +0530

    31th day
    Signed-off-by: LightxAman <amanxlight@gmail.com>

--- Day31.py
+++ Day31.py
@@ -1,5 +1,7 @@
 # Palindrome Program
 
+# Example 1
+
 str = 'JaVaJ'
 strstr = str.casefold()
 
@@ -10,3 +12,11 @@ if list(str) == list(rev):
     print("PALINDROME !")
 else:
     print("NOT PALINDROME !")
+
+# Example 2
+
+string = input("Enter a letter:")
+if string == string[::-1]:
+    print("The letter is a palindrome")
+else:
+    print("The letter is not a palindrome")
