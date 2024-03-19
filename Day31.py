ea61b4cdeae78e05c3a593bb5d62a6feded70c9e
LightxAman <amanxlight@gmail.com>
2024-03-19 20:04:05 +0530

    31th day
    Signed-off-by: LightxAman <amanxlight@gmail.com>

--- Day31.py
+++ Day31.py
@@ -20,3 +20,17 @@ if string == string[::-1]:
     print("The letter is a palindrome")
 else:
     print("The letter is not a palindrome")
+
+# Example 3 using for loop
+
+Num = int(input("Enter a value:"))
+Temp = num
+Rev = 0
+while num > 0:
+    dig = num % 10
+    revrev = rev * 10 + dig
+    numnum = num // 10
+if temp == rev:
+    print("This value is a palindrome number!")
+else:
+    print("This value is not a palindrome number!")
