a9f15bf390ba2eb3bfd673f068d57bfcca340048
LightxAman <amanxlight@gmail.com>
2024-03-19 20:02:18 +0530

    31th day
    Signed-off-by: LightxAman <amanxlight@gmail.com>

---
+++ Day31.py
@@ -0,0 +1,12 @@
+# Palindrome Program
+
+str = 'JaVaJ'
+strstr = str.casefold()
+
+# This string is reverse.
+rev = reversed(str)
+
+if list(str) == list(rev):
+    print("PALINDROME !")
+else:
+    print("NOT PALINDROME !")
