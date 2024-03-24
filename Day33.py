9e13ea88e9661a5daf6ffa7bbb6fc25f40bcc446
LightxAman <amanxlight@gmail.com>
2024-03-24 19:06:14 +0530

    day 34
    Signed-off-by: LightxAman <amanxlight@gmail.com>

---
+++ Day33.py
@@ -0,0 +1,7 @@
+# Recursion in Python
+
+# Recursion is the process of defining something in terms of itself
+# A physical world example would be to place two parallel mirrors facing each other.
+# Any object in between them would be reflecting recursively.
+
+# Python recursive function
