108e15fcbb667663596ea3a6c72309024e1c1dfb
LightxAman <amanxlight@gmail.com>
2024-03-24 19:08:58 +0530

    day 34
    Signed-off-by: LightxAman <amanxlight@gmail.com>

--- Day33.py
+++ Day33.py
@@ -5,3 +5,15 @@
 # Any object in between them would be reflecting recursively.
 
 # Python recursive function
+
+# Example 1 : Factorial
+# factorial(7) = 7*6*5*4*3*2*1
+# factorial(6) = 6*5*4*3*2*1
+# factorial(5) = 5*4*3*2*1
+# factorial(4) = 4*3*2*1
+# factorial(3) = 3*2*1
+# factorial(2) = 2*1
+# factorial(1) = 1
+# factorial(0) = 1
+
+# factorial(n) = n * factorial(n-1)
