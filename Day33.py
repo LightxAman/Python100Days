7f005928dc04d2c18a2c28810e2985a0018f98a7
LightxAman <amanxlight@gmail.com>
2024-03-24 19:10:38 +0530

    day 34
    Signed-off-by: LightxAman <amanxlight@gmail.com>

--- Day33.py
+++ Day33.py
@@ -17,3 +17,12 @@
 # factorial(0) = 1
 
 # factorial(n) = n * factorial(n-1)
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
+print(factorial(5))
